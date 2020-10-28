69ceb58806dfd589dac4c6761350e12303000acc
Jake Allen <jake_allen1@baylor.edu>
2020-10-27 22:28:01 -0500

    Added sam's item/list changes

--- src/constructs/Item.java
+++ src/constructs/Item.java
@@ -11,6 +11,39 @@ public class Item {
 		this.store = storeName;
 	}
 	
+	//Sam added this
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((store == null) ? 0 : store.hashCode());
+		return result;
+	}
+
+	//also sam added this
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (store == null) {
+			if (other.store != null)
+				return false;
+		} else if (!store.equals(other.store))
+			return false;
+		return true;
+	}
+	
 	public String getName() {
 		return this.name;
 	}
@@ -21,5 +54,12 @@ public class Item {
 	
 	public String getStore() {
 		return this.store;
+	}
+	
+	public boolean isCrossedOff(){
+		if(quantity == 0){
+			return true;
+		}
+		return false;
 	}
 }
