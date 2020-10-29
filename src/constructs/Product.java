a7842f7c47b472cb2588bed701d5badd0203e894
Evan-Berryman <34588554+Evan-Berryman@users.noreply.github.com>
2020-10-28 19:26:49 -0500

    Brought Product up to date

--- src/constructs/Product.java
+++ src/constructs/Product.java
@@ -2,7 +2,38 @@ package constructs;
 
 import java.util.Vector;
 
+/*
+ * The product represents a product in the store,
+ * NOT an item on a list
+ */
+
 public class Product {
-	String name;
-	Vector<String> keywords;
+	String brand, type, extra;
+	
+	public Product(String[] split) {
+		brand = split[0];
+		type = split[1];
+		extra = split[2];
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getExtra() {
+		return extra;
+	}	
+	
+	public String toString() {
+		return brand + " " + type + " " + extra;
+	}
+	
+	public String[] getVector(){
+		String[] ret = {brand, type, extra};
+		return ret;
+	}
 }
