2b0769359594a43268283a968a4480c15bb2dd53
Fireman89 <44931483+Fireman89@users.noreply.github.com>
2020-11-28 14:02:51 -0600

    Holds Stores - allows reading certain # products

---
+++ src/storage/StoreStorage.java
@@ -0,0 +1,37 @@
+package storage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+
+import constructs.Product;
+
+public class StoreStorage {
+	Vector<Store> stores;
+	public StoreStorage(String filename) {
+		stores = new Vector<Store>();
+		Scanner scanner = null;
+		try {
+			String line;
+			scanner = new Scanner(new File(filename));
+			while(scanner.hasNextLine()) {
+				line = scanner.nextLine();
+				String[] split = line.split(",");
+				String storeName = split[0];
+				int itemsInStore = Integer.parseInt(split[1]);
+				Store s = new Store(storeName);		
+				for (int i = 0; i < itemsInStore; i++) {
+					String[] productInfo = scanner.nextLine().split(",");
+					s.addProduct(new Product(productInfo));	//PLEASE BE SURE INFO IS CORRECT LENGTH
+				}				
+				stores.add(s);
+			}	
+		} catch (FileNotFoundException e) {
+			System.out.println("StoreStorage error: "+ e.getMessage());
+		}
+	}
+	
+	public Vector<Store> getStores(){return stores;}
+	
+}
